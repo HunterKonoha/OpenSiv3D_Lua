75416df2438be5a8c4ebe82ef60baee7b610d5e0
konoha <konoha_hunter@yahoo.co.jp>
2021-08-15 23:03:15 +0900

    内部文字列の変換をtemplateからオーã関数ーロードに変更(適した方æ実装に要変更？)

--- OpenSiv3D_lua/src/LuaForOpenSiv3D/InternalStrings.cpp
+++ OpenSiv3D_lua/src/LuaForOpenSiv3D/InternalStrings.cpp
@@ -1,12 +1,10 @@
 #include <LuaForOpenSiv3D/InternalStrings.h>
 #include <Siv3D/Unicode.hpp>
 
-template<>
-std::string s3d::LuaScript::Internal::convertString<std::string, s3d::String>(const s3d::String& string) {
+std::string s3d::LuaScript::Internal::convertString(const s3d::String& string) {
 	return string.narrow();
 }
 
-template<>
 s3d::String s3d::LuaScript::Internal::convertString(const std::string& string) {
 	return s3d::Unicode::Widen(string);
 }
